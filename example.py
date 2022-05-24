02d13ae95a2fe515473f7361a1af90f9388c7df7
anneliret <91963804+anneliret@users.noreply.github.com>
2022-05-24 10:25:28 +0100

    Update example.py

--- example.py
+++ example.py
@@ -13,12 +13,12 @@ recommender.load_model("models/trained_model.pk")
 
 
 #query = 'New dropfrom eu to carrier pole unable pu t to drop from carrier to dp as it goes over train tracks. An di cant do this safely.'
-#query = 'I cannot complete this task because I have run out of timeithin my scheduled hours. The end customer cannot use the srvice. The work outstanding is Track.'
+#query = 'IncompleteReason: I have run out of timeithin my scheduled hours. The end customer cannot use the srvice. The work outstanding is Track.'
 #expl  = recommender.explain (query)
 
-query = 'I cannot complete this task because Faulty fibre port ( no s pares ) causing noisy line. Waiting on lums case 184241, spo ke to dcoe,expected to be resolved 5/5/18. Please pass to CS S queue Id Na. The line has been proven good to the PCP. Ear th contact was detected towards the end customer.'
+query = 'IncompleteReason: Faulty fibre port ( no s pares ) causing noisy line. Waiting on lums case 184241, spo ke to dcoe,expected to be resolved 5/5/18. Please pass to CS S queue Id Na. The line has been proven good to the PCP. Ear th contact was detected towards the end customer.'
 
-#query = "I cannot complete this task because of a hazard owned by Openreach / third party. Dp damaged by fire A1024 13416129 manager informed  is preventing further work. "
+#query = "IncompleteReason: a hazard owned by third party. Dp damaged by fire A1024 13416129 manager informed  is preventing further work. "
 print (recommender.predict([query]))
 
 print (recommender.predict_proba([query]))
